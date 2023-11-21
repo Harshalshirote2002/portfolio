993f6d947a7e6b8fcbcab46f0c2094fc1a72568d
Harshal Shirote <harshalshirote2002@gmail.com>
2023-11-21 23:32:44 +0530

    Add details

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,32 +1,72 @@
-export default function Content() {
+const works = [
+  {
+    imagePath: "./src/images/CV-maker.png",
+    title: "CV-Maker App",
+    link: "https://github.com/Harshalshirote2002/CVmaker",
+    description:
+      "An user-friendly tool built that enables individuals to effortlessly create and customize their CV online",
+  },
+  {
+    imagePath: "./src/images/knights-travails.png",
+    title: "Knights Travails path visualized",
+    link: "https://harshalshirote2002.github.io/knights-travails/",
+    description:
+      "Finds the Knight's shortest path from start to end node using the Breadth First Search algorithm",
+  },
+  {
+    imagePath: "./src/images/ANPR.png",
+    title: "Automatic Number Plate Recognition",
+    link: "https://github.com/Harshalshirote2002/ANPR",
+    description:
+      "Incorporates number plate localization based solely upon morphological processing of the input image",
+  },
+  {
+    imagePath: "./src/images/image-stitcher.png",
+    title: "Image Stitcher",
+    link: "https://github.com/Harshalshirote2002/image-stitching",
+    description: "Combines two input images together to form a panoramic view",
+  },
+  {
+    imagePath: "./src/images/weather-app.png",
+    title: "Weather App",
+    link: "https://github.com/Harshalshirote2002/weather-app",
+    description: "Displays real-time weather data for user-specified locations",
+  },
+  {
+    imagePath: "./src/images/todo-list.png",
+    title: "Todo-list App",
+    link: "https://github.com/Harshalshirote2002/todo-list",
+    description:
+      "A streamlined web app for managing tasks and to-do lists with user-friendly features",
+  },
+];
 
+function AddWork() {
+  return works.map((work) => (
+    <div key={work.title} className="work-card">
+      <img className="work-card-screenshot" src={work.imagePath} />
+      <div className="work-card-content">
+        <div className="work-card-content-title">{work.title}</div>
+        <a
+          rel="noreferrer"
+          target="_blank"
+          className="work-card-content-icons"
+          href={work.link}
+        >
+          <img src="./src/images/open.svg" />
+        </a>
+        <div className="work-card-content-description">{work.description}</div>
+      </div>
+    </div>
+  ));
+}
+
+export default function Content() {
   return (
     <div className="work">
       <h1>My Work</h1>
       <div className="work-content">
-        <div className="work-card">
-          <div className="work-card-screenshot">screenshot</div>
-          <div className="work-card-content">
-            <div className="work-card-content-title">some title</div>
-            <div className="work-card-content-icons">icons</div>
-            <div className="work-card-content-description">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis
-              atque exercitationem magni accusantium suscipit sapiente velit?
-            </div>
-          </div>
-        </div>
-        
-        <div className="work-card">
-          <div className="work-card-screenshot">screenshot</div>
-          <div className="work-card-content">
-            <div className="work-card-content-title">some title</div>
-            <div className="work-card-content-icons">icons</div>
-            <div className="work-card-content-description">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis
-              atque exercitationem magni accusantium suscipit sapiente velit?
-            </div>
-          </div>
-        </div>
+        <AddWork />
       </div>
     </div>
   );
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,21 +1,25 @@
-export default function Header(){
-    return (
-        <header>
+export default function Header() {
+  return (
+    <header>
       <div className="about-image">
         <h1>Harshal Shirote</h1>
-        </div>
+      </div>
       <div className="about">
-        <h2>About me</h2>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum explicabo
-        quidem itaque, velit illum laborum eius inventore? Velit ipsa odit
-        nesciunt ex magni enim id officia corrupti, dolorem qui a. Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. A quaerat ad, ex accusamus
-        tempora quasi rerum labore. Beatae eos velit,
+        <h2>About me</h2>I am currently pursuing a Bachelor of Technology in
+        Data Science and Artificial Intelligence at IIIT, Naya Raipur. My focus
+        and passion lie in the Computer Vision domain. Actively seeking
+        opportunities in this field, I am eager to apply my skills and
+        contribute to advancements.
         <div className="about-icons">
-          <a rel="noreferrer" href="https://github.com/Harshalshirote2002" target="_blank">
+          <a
+            rel="noreferrer"
+            href="https://github.com/Harshalshirote2002"
+            target="_blank"
+          >
             <i className="devicon-github-original"></i>
           </a>
-          <a rel="noreferrer"
+          <a
+            rel="noreferrer"
             href="https://linkedin.com/in/harshal-shirote-07274223a/"
             target="_blank"
           >
@@ -24,5 +28,5 @@ export default function Header(){
         </div>
       </div>
     </header>
-    );
+  );
 }
