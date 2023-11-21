1d74ada1c3b819d8172fe8cdce07194379a113c9
Harshal Shirote <harshalshirote2002@gmail.com>
2023-11-21 23:51:29 +0530

    Fix image imports

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,39 +1,47 @@
+import image1 from "../images/CV-maker.png";
+import image2 from "../images/knights-travails.png";
+import image3 from "../images/ANPR.png";
+import image4 from "../images/image-stitcher.png";
+import image5 from "../images/weather-app.png";
+import image6 from "../images/todo-list.png";
+import image7 from "../images/open.svg";
+
 const works = [
   {
-    imagePath: "./src/images/CV-maker.png",
+    imagePath: image1,
     title: "CV-Maker App",
     link: "https://github.com/Harshalshirote2002/CVmaker",
     description:
       "An user-friendly tool built that enables individuals to effortlessly create and customize their CV online",
   },
   {
-    imagePath: "./src/images/knights-travails.png",
+    imagePath: image2,
     title: "Knights Travails path visualized",
     link: "https://harshalshirote2002.github.io/knights-travails/",
     description:
       "Finds the Knight's shortest path from start to end node using the Breadth First Search algorithm",
   },
   {
-    imagePath: "./src/images/ANPR.png",
+    imagePath: image3,
     title: "Automatic Number Plate Recognition",
     link: "https://github.com/Harshalshirote2002/ANPR",
     description:
       "Incorporates number plate localization based solely upon morphological processing of the input image",
   },
   {
-    imagePath: "./src/images/image-stitcher.png",
+    imagePath: image4,
     title: "Image Stitcher",
     link: "https://github.com/Harshalshirote2002/image-stitching",
     description: "Combines two input images together to form a panoramic view",
   },
   {
-    imagePath: "./src/images/weather-app.png",
+    imagePath: image5,
     title: "Weather App",
     link: "https://github.com/Harshalshirote2002/weather-app",
     description: "Displays real-time weather data for user-specified locations",
   },
   {
-    imagePath: "./src/images/todo-list.png",
+    imagePath: image6,
     title: "Todo-list App",
     link: "https://github.com/Harshalshirote2002/todo-list",
     description:
@@ -53,7 +61,7 @@ function AddWork() {
           className="work-card-content-icons"
           href={work.link}
         >
-          <img src="./src/images/open.svg" />
+          <img src={image7} />
         </a>
         <div className="work-card-content-description">{work.description}</div>
       </div>
